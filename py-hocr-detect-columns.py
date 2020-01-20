fab5f4fdcefc7349fc5459ed203ec097c022134b
Nicholas Wolf <nicholas.wolf@nyu.edu>
2020-01-20 04:50:57 -0500

    Filling out remaining items to identify indents and keep lines

--- py-hocr-detect-columns.py
+++ py-hocr-detect-columns.py
@@ -4,14 +4,19 @@ from sklearn.cluster import KMeans
 from math import sqrt
 from statistics import mean
 from PIL import Image, ImageOps, ImageDraw
+import re
+import json
 
 
 # Page Array Structure
 # col 0 = ID number of line
 # col 1-4 = bbbox x1, y1, x2, y2
 # col 5 = 0:col line; 1: indent line; 2:kill line
+# col 6 = 1:col 1; 2: col2 (column assignment)
+# col 7 = 0:ok to append, 1: not ok (likely gap)
 
-def imagebuilder(r, col_locations, image_filename, std):
+
+def imagebuilder(r, col_locations, image_filename, std, gap_locations):
     pageimg = Image.open(image_filename)
     overlay = ImageDraw.Draw(pageimg, 'RGBA')
 
@@ -35,6 +40,14 @@ def imagebuilder(r, col_locations, image_filename, std):
                         fill=(255, 1, 255, 100))
         overlay.line([(loc, 0), (loc, pageimg.size[1])],
                      fill=(0, 0, 0, 127), width=4)
+
+    # Draw the gap lines
+
+    for gap_location in gap_locations:
+        overlay.line([(0, gap_location), (pageimg.size[0], gap_location)],
+                     fill=(0, 0, 0, 127), width=4)
+
+
     pageimg.show()
 
 def load_hocr_lines(filepath):
@@ -43,17 +56,24 @@ def load_hocr_lines(filepath):
     for line in rawhtml.html.body.div.find_all('span'):
         line_list = []
         if line['class'][0] == 'ocr_line':
-            line_list.append(int(line['id'].split('_')[2]))
-            line_list += [int(i) for i in line['title'].split(';')[0].split(' ')[1:]]
-            line_list.append(0)
-            page_array.append(line_list)
+            
+            # We do a check to weed out any junk lines with no alphanumeric characters
+            
+            check_for_chars = []
+            for child in line.children:
+                check_for_chars.append(child.string)
+            check_for_chars = ' '.join(check_for_chars)
+            if re.search(r'[A-Za-z0-9]+', check_for_chars):
+                line_list.append(int(line['id'].split('_')[2]))
+                line_list += [int(i) for i in line['title'].split(';')[0].split(' ')[1:]]
+                line_list += [0,0,0]
+                page_array.append(line_list)
     return np.array(page_array)
 
 
 
 path = '/Users/Nicholas/Desktop/TEMP-NYU-DIRECTORIES-PROJECT-OFFLINE/test-verification-files/1875.4b4b2b90-317a-0134-6800-00505686a51c/198.56789039.6c645950-5cec-0134-2c5e-00505686a51c_cropped.hocr'
 r = load_hocr_lines(path)
-
 
 ##
 #   Pass to find our likey column locations
@@ -83,23 +103,78 @@ std1 = sqrt(mean([((i - top_cands[0])**2) for i in r[:, 1]]))
 std2 = sqrt(mean([((i - top_cands[1])**2) for i in r[:, 1]]))
 for i in range(len(r)):
     if abs(r[i:i + 1, 1] - top_cands[0]) < (std1 / 32) or abs(r[i:i + 1, 1] - top_cands[1]) < (std2 / 32):
-        pass
+        # Col 1 identification:
+        if abs(r[i:i + 1, 1] - top_cands[0]) < (std1 / 32):
+            r[i:i + 1, 6] = 1
+        # Col 2 identification:
+        elif abs(r[i:i + 1, 1] - top_cands[1]) < (std2 / 32):
+            r[i:i + 1, 6] = 2
     else:
         r[i:i+1,5] = 2
-    if r[i:i + 1, 5] == 2 and (r[i:i + 1, 1] - (top_cands[0] + (std1 /8)) < 0 or r[i:i + 1, 1] - (top_cands[1] + (std2 /8 )) < 0):
+    #Adding back in indents
+    if r[i:i + 1, 5] == 2 and r[i:i + 1, 1] - (top_cands[0] + (std1 /8)) < 0:
+        #Col 1 indent identification
         r[i:i + 1, 5] = 1
+        r[i:i + 1, 6] = 1
+    elif r[i:i + 1, 5] == 2 and r[i:i + 1, 1] - (top_cands[1] + (std2 /8 )) < 0:
+        # Col 2 indent identification
+        r[i:i + 1, 5] = 1
+        r[i:i + 1, 6] = 2
+    #Re-eliminating anything to left of column 1
     if r[i:i+1, 1] < (min(top_cands) - (std1/32)):
-        r[i:i + 1, 5] = 2
+       r[i:i + 1, 5] = 2
  
 ##
 #   Pass to find lines flush with a column whose y vals make them unlikely to be in the page block for entries
 ##
 
+reduced_array = r[r[:,5] !=2]
+sorted_y_array = np.sort(reduced_array.view('i8,i8,i8,i8,i8,i8,i8,i8'), order=['f2'], axis=0).view(np.int)
+
+half_page = len(sorted_y_array)//2
+hp_line_y = sorted_y_array[half_page:half_page+1,2]
+gap = float(r[-2:-1,2]*.10)
+entry_density = len([i for i in sorted_y_array[:,2] if abs(i - sorted_y_array[half_page:half_page+1,2]) < gap/2])
 
 
+for i in range(len(sorted_y_array)):
+    proximate_lines = [yval for yval in sorted_y_array[:,2] if abs(yval - sorted_y_array[i:i+1,2]) < gap/2]
+    if len(proximate_lines) - 1 < entry_density/2:
+        top_line_proximate_lines = [yval for yval in sorted_y_array[:, 2] if yval - sorted_y_array[i:i + 1, 2] < gap \
+                                    and yval - sorted_y_array[i:i + 1, 2] > 0]
+        bottom_line_proximate_lines = [yval for yval in sorted_y_array[:, 2] if \
+                                    sorted_y_array[i:i + 1, 2] - yval < gap and sorted_y_array[i:i + 1, 2] - yval > 0]
+        if len(top_line_proximate_lines) > entry_density or len(bottom_line_proximate_lines) > entry_density:
+            pass
+        else:
+            sorted_y_array[i:i+1,5] = 2
+        
+##
+#   Pass to look for missing lines
+##
 
+line_only_array = sorted_y_array[sorted_y_array[:,5] != 2]
+sorted_line_only_array = np.sort(line_only_array.view('i8,i8,i8,i8,i8,i8,i8,i8'), order=['f6','f2'], axis=0).view(np.int)
+sample_lines = sorted_line_only_array[int(len(sorted_line_only_array)*.15):int(len(sorted_line_only_array)*.30),:]
+gaps = []
+
+for i in range(len(sample_lines)):
+    try:
+        gaps.append(int(sample_lines[i+1:i+2,2] - sample_lines[i:i+1,2]))
+    except:
+        pass
+
+average_line_gap = sum(gaps) // len(sample_lines)
+gap_locations = []
+for i in range(len(sorted_line_only_array)):
+    try:
+        if int(sorted_line_only_array[i + 1:i + 2, 2] - sorted_line_only_array[i:i + 1, 2]) > average_line_gap*1.95:
+            #print("Likely line missing after line: ", i)
+            gap_locations.append(sorted_line_only_array[i:i + 1, 2] + average_line_gap*1.5)
+    except:
+        pass
 
 
 jpeg_path = '/Users/Nicholas/Desktop/TEMP-NYU-DIRECTORIES-PROJECT-OFFLINE/test-verification-files/1875.4b4b2b90-317a-0134-6800-00505686a51c/cropped-jpegs/198.56789039.6c645950-5cec-0134-2c5e-00505686a51c_cropped.jpeg'
 #print(r[:,5])
-imagebuilder(r, top_cands, jpeg_path, std)
+imagebuilder(sorted_line_only_array, top_cands, jpeg_path, std, gap_locations)
